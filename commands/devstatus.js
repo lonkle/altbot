dde9fd52854e1cb65b6dd8f212712a267a62eada
Nythepegasus <nythepegasus84@gmail.com>
2021-08-25 08:12:40 -0400

    Fixed screenshot for server statuses using different node package

--- commands/devstatus.js
+++ commands/devstatus.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-var screenshot = require("node-server-screenshot");
+var webshot = require("webshot");
 const fs = require('fs');
 module.exports = {
 	name: 'devstatus',
@@ -9,7 +9,7 @@ module.exports = {
 	execute(message, args) {
 		message.channel.send("Fetching status... <a:loading:718190657579253782>")
 			.then(msg => {
-				screenshot.fromURL("https://developer.apple.com/system-status/", "dstatus.jpeg", { width: 2560, height: 1440, clip: { x: 160, y: 80, width: 1000, height: 700 } }, function () {
+				webshot.fromURL("https://developer.apple.com/system-status/", "dstatus.jpeg", { screenSize: { width: 1920, height: 1080 }, { shotSize: { width: 984, height: 708 }, shotOffset: { left: 467, top: 98 } } }, function () {
 					msg.edit("https://developer.apple.com/system-status/");
 					message.channel.send({ files: ["dstatus.jpeg"] });
 					setTimeout(() => {
